1a798ef1840cbf86e7585bd2a757791b5b82ff2b
Adam Říha <64468662+admri@users.noreply.github.com>
2025-11-15 19:34:14 +0100

    Add GPL header

--- MultiSetMap.h
+++ MultiSetMap.h
@@ -1,3 +1,22 @@
+/* MultiSetMap is a header-only library which defines a generic map with sets as values
+ * Copyright (C) 2025  <name of author>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ * For more information regarding this project please contact me at rihaadam1<at>seznam.cz.
+ */
+
 #ifndef MULTI_SET_MAP_H
 #define MULTI_SET_MAP_H
 
